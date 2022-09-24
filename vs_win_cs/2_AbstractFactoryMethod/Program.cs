3bc8fca3fb80768d47e126b70d07ce56d32315a2
SOONYEONG JUNG <soonyoung87@gmail.com>
2022-09-24 11:32:43 +0900

    Add abstract factory pattern example.

---
+++ vs_win_cs/2_AbstractFactoryMethod/Program.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _2_AbstractFactoryMethod
+{
+    public interface IProduct
+    {
+        String GetName();
+        bool SetPrice(double price);
+    }
+
+    public class Phone : IProduct
+    {
+        private double _price;
+
+        public string GetName()
+        {
+            return "Apple TouchPad";
+        }
+
+        public bool SetPrice(double price)
+        {
+            _price = price;
+            return true;
+        }
+    }
+
+    public abstract class ProductAbstractFactory
+    {
+        protected abstract IProduct MakeProduct();
+
+        public IProduct GetObject()
+        {
+            return this.MakeProduct();
+        }
+    }
+
+    public class PhhoneConcreteFactory : ProductAbstractFactory
+    {
+        protected override IProduct MakeProduct()
+        {
+            IProduct product = new Phone();
+            product.SetPrice(30);
+            return product;
+        }
+    }
+
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            ProductAbstractFactory factory = new PhhoneConcreteFactory();
+            IProduct product = factory.GetObject();
+            Console.WriteLine("Name: " + product.GetName());
+        }
+    }
+}
